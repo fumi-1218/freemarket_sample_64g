706acd78a6fa3d1130e18b19cd5ce65ac254cacd
masakiosawa <rssgboy1@gmail.com>
2019-12-11 13:03:59 +0900

    テストコードエラー

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :user do
     nickname {"g64"}
-    email {"g64@gmail.com"}
+    
   end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,14 +5,14 @@ describe User do
   
     it "is invalid without a nickname" do
 
-      # user = build(:user, nickname: "")
-      # user.valid?
-      # expect(user.errors[:nickname]).to include("を入力してください")
+      user = build(:user, nickname: "")
+      user.valid?
+      expect(user.errors[:nickname]).to include("を入力してください")
     end
     it "is invalid without a email" do
-      user = build(:user, email: nil)
-      user.valid?
-      expect(user.errors[:email]).to include("を入力してください")
+      # user = build(:user, email: nil)
+      # user.valid?
+      # expect(user.errors[:email]).to include("を入力してください")
     end
   end
 end
