852c4252a5638167ba1b413528d274c845d1eb65
fumi <sasakiyoshifumi@fuminoMacBook-Pro.local>
2021-03-26 17:08:16 +0900

    gemの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,9 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # 下の一行はQiitaに記載したのでコメントアウト
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
+  
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
